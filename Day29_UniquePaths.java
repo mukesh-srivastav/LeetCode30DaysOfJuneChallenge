7d46fbd9bf0686f16d5cad03f1df66a77d026313
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2020-07-01 02:29:27 +0530

    Added Day28_ReconstructItinerary.java, Day29_UniquePaths.java, Day30_WordSearch2.java

---
+++ Day29_UniquePaths.java
@@ -0,0 +1,19 @@
+public class Day29_UniquePaths {
+    public int uniquePaths(int m, int n) {
+        if (n < 1 || m < 1)
+            return 0;
+        int a[][] = new int[m][n];
+        
+        for (int i=0; i<m; i++)
+            a[i][0] = 1;
+        
+        for (int i=0; i<n; i++)
+            a[0][i] = 1;
+        
+        for (int i=1; i<m; i++)
+            for (int j=1; j<n; j++)
+                a[i][j] = a[i][j-1] + a[i-1][j];
+        
+        return a[m-1][n-1];
+    } 
+}
